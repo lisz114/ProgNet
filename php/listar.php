61ae84413e38d9096411a354cffa9041b34a9f3a
Elisa <vitoriaelisa114@gmail.com>
2024-12-09 11:59:48 -0300

    php dnv

---
+++ php/listar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    <?php
+    include_once("db.php");
+    include_once("pessoa_da.php")
+
+    getUsuario();
+    ?>        
+    </pre>
+</body>
+</html>
